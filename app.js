4e8151cf42a90361872b32417c52623a08fa42ef
MD. Shimul <mdshemul480@gmail.com>
2022-10-23 18:13:26 +0600

    adding style

--- app.js
+++ app.js
@@ -16,10 +16,8 @@ const treeData = {
 };
 
 const tree = d3.hierarchy(treeData);
-
 const treeStructure = d3.tree().size([900, 700]);
 const treeInfo = treeStructure(tree);
-
 const dataInfo = treeInfo.descendants();
 
 const svg = d3.select('svg').attr('width', 1000).attr('height', 900);
@@ -34,36 +32,57 @@ const connections = svg
     'd',
     d3
       .linkHorizontal()
-      .x((d) => d.y + 10)
+      .x((d) => d.y + 15)
       .y((d) => d.x + 12)
   );
 
-const circles = svg.append('g').selectAll('circle').data(dataInfo);
-
-circles
-  .enter()
-  .append('circle')
-  .attr('cx', function (d) {
-    return d.y + 10;
-  })
-  .attr('cy', function (d) {
-    return d.x + 12;
-  })
-  .attr('fill', 'red')
-  .attr('r', 10);
-
-const texts = svg
-  .append('g')
-  .selectAll('text')
+const notes = svg
+  .selectAll('g')
   .data(dataInfo)
   .enter()
+  .append('g')
+  .attr('transform', (d) => `translate(${d.y + 15},${d.x + 12})`);
+
+notes.append('circle').attr('r', 6.5);
+notes
   .append('text')
+  .text((d) => d.data.name)
   .attr('x', function (d) {
-    return d.y + 25;
+    return d.children ? -13 : 13;
   })
-  .attr('y', function (d) {
-    return d.x + 15;
+  .attr('dy', '.35em')
+  .attr('text-anchor', function (d) {
+    return d.children ? 'end' : 'start';
   })
-  .text((d) => {
-    return d.data.name;
-  });
+  .style('fill-opacity', 1)
+  .attr('fill', '#fff');
+
+// const circles = svg.append('g').selectAll('circle').data(dataInfo);
+
+// circles
+//   .enter()
+//   .append('circle')
+//   .attr('cx', function (d) {
+//     return d.y + 10;
+//   })
+//   .attr('cy', function (d) {
+//     return d.x + 12;
+//   })
+//   .attr('fill', 'red')
+//   .attr('r', 10);
+
+// const texts = svg
+//   .append('g')
+//   .selectAll('text')
+//   .data(dataInfo)
+//   .enter()
+//   .append('text')
+//   .attr('x', function (d) {
+//     return d.y - 56;
+//   })
+//   .attr('y', function (d) {
+//     return d.x + 4;
+//   })
+//   .text((d) => {
+//     return d.data.name;
+//   });
